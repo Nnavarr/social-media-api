6f9760d99a3f96e7fcad4eb4948d0b04f14d150a
eon <noe.navarro1019@gmail.com>
2022-03-09 20:53:39 -0700

    created single route for id specific routes for User model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,12 +16,20 @@ const userController = {
     User.create(body)
     .then(dbUserData => res.json(dbUserData))
     .catch(err => res.status(400).json(err));
+  },
+
+  // update user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbUserData => {
+        if(!dbUserData) {
+          res.status(400).json({ message: 'User not found' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   }
-
-
 }
-
-
-
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 
-const { getAllUsers, createUser } = require('../../controllers/user-controller');
+const { getAllUsers, createUser, updateUser } = require('../../controllers/user-controller');
 
 // assign routes
 router
   .route('/')
   .get(getAllUsers)
-  .post(createUser);
-  
+  .post(createUser)
+
+// specific id routes /api/users/:id
+router
+  .route('/:id')
+  .put(updateUser);
+
+
 // export router
 module.exports = router;
